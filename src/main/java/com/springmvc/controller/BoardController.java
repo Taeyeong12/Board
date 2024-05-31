a8509582588711872d6c98d1a6a80233d660d7c2
Taeyeong12 <tkddn37900@gmail.com>
2024-05-31 17:51:48 +0900

    로그인관련

--- src/main/java/com/springmvc/controller/BoardController.java
+++ src/main/java/com/springmvc/controller/BoardController.java
@@ -2,13 +2,25 @@ package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
 	
 	@GetMapping("/list")
 	public String list() {
+		
 		return "list";
 	}
 
+	@GetMapping("/board")
+	public String board(@RequestParam("id")int id) {
+		
+		System.out.println("id : " + id );
+		
+		
+		//id에 해당하는 게시물을 읽어온다.
+		//id에 해당하는 게시물의 조회수도 1증가한다.
+		return"board";
+	}
 }
--- src/main/java/com/springmvc/controller/UserController.java
+++ src/main/java/com/springmvc/controller/UserController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
 	
+	//회원가입폼 페이지
 	@GetMapping("/userRegForm")
 	public String userRegForm() {
 		
 		return "userRegForm";
 	}
 	
+	//회원가입한 정보를 받는 페이지
 	@PostMapping("/userReg")
 	public String userReg(
 			@RequestParam("name")String name,
@@ -22,11 +24,47 @@ public class UserController {
 			@RequestParam("password")String password
 			) {
 		
+		//회원정보를 저장
 		System.out.println("name :" + name);
 		System.out.println("email : " + email);
-		System.out.println("passwrod" + password);
+		System.out.println("passwrod : " + password);
 		
 		return "redirect:/welcome";
 	}
 	
+	//회원가입 환영페이지
+	@GetMapping("/welcome")
+	public String welcome() {	
+		
+		return "welcome";
+	}
+	
+	@GetMapping("/loginform")
+	public String loginform() {
+		
+		return"loginform";
+	}
+	
+	
+	@PostMapping("/login")
+	public String login(
+			@RequestParam("email")String email,
+			@RequestParam("password")String password
+			) {
+		
+		//email에 해당하는 회원 정보를 읽어온 후
+		//아이디 암호가 맞다면 세션에 회원정보를 저장한다.
+		//welcome페이로 이동한다.
+		
+		System.out.println("email : " + email );
+		System.out.println("password : " + password );
+
+		return"redirect:/list";
+	}
+	
+	@GetMapping("/logout")
+	public String lougout() {
+		//세션에서 회원정보를 삭제
+		return"redirect:/list";
+	}
 }
